60e9aa887cb7ce5b268725ca1ee1c16dd6e1daa5
python-boi! <cmd350.19@gmail.com>
2024-10-01 20:43:32 -0500

    streamlit thingy

---
+++ Project9_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="3 Question Quiz", page_icon=":small_airplane:", layout="wide")
+
+
+st.subheader("Hello, i am a young child who trys to reach his goals")
+st.title("Im only a kid wants to either be a pilot:small_airplane:, or a software developer/engineer:laptop:.")
+st.write("Click this link to go to my Math quiz!: https://www.online-python.com/W0Z4zrfJL2")
