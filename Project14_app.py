40f6a1c00a569142781a30e4facae6272d3331fe
python-boi! <cmd350.19@gmail.com>
2024-10-04 21:33:19 -0500

    Update Project14_app.py
    Number Generator

--- Project14_app.py
+++ Project14_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import random
 st.set_page_config(page_title="Number Generator", page_icon=":small_airplane:", layout="wide")
 st.write("The number you enter will be selected for the generator for example 1 - (input of user)")
-number = st.number_input("Enter a Number: ")
-st.write(f"The number you selected is {number}")
-st.write(f"1 - {number}")
-x = random.randint(1,{number})
+y = st.number_input("Enter a Number: ")
+st.write(f"The number you selected is {y}")
+st.write(f"1 - {y}")
+x = random.randint(1,{y})
 st.write(f"The selected number from the generator is {x}")
