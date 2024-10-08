3efb6e11b62364b374ca154e864b64302b394464
python-boi! <cmd350.19@gmail.com>
2024-10-07 20:40:35 -0500

    Update Project10(AD. Math Quiz).py
    math!!1!1

--- Project10(AD. Math Quiz).py
+++ Project10(AD. Math Quiz).py
@@ -7,33 +7,35 @@ x = random.randint(0,10)
 y = random.randint(0,10)
 print(f"What is {x} + {y}")
 z = int(input("Answer here: "))
-if (z) == (x) + (y):
+if z == x + y:
     print(f"{Fore.BLUE}{Back.LIGHTGREEN_EX}Correct! Your score increased by 1!")
-    (score) += 1
-    print("")
+    score += 1
+    print(f"{Fore.RESET}{Back.RESET}")
 else:
     print(f"{Fore.LIGHTRED_EX}{Back.LIGHTBLACK_EX}Wrong. the correct answer was {x + y}!")
 a = random.randint(0,10)
 b = random.randint(0,10)
 print(f"{Fore.RESET}{Back.RESET}What is {a} * {b}?")
 c = int(input("Answer Here: "))
-if (c) == (a) * (b):
-    print("")
+if c == a * b:
     print(f"{Fore.BLUE}{Back.LIGHTGREEN_EX}Correct! Your score has updated!")
+    print(f"{Fore.RESET}{Back.RESET}")
     score += 1
 else:
     print(f"{Fore.LIGHTRED_EX}{Back.LIGHTBLACK_EX}Wrong, the correct  answer was {a * b}. Your score in total was {score}!")
+    print(f"{Fore.RESET}{Back.RESET}")
 r = random.randint(0,10)
 f = random.randint(0,10)
 print(f"{Fore.RESET}{Back.RESET}What is {r} * {f}?")
 t = int(input("Answer Here: "))
 if (t) == (r) * (f):
-    (score) += 1
+    score += 1
     print(f"{Fore.BLUE}{Back.LIGHTGREEN_EX}Correct! Your score in total was {score}!")
+    print(f"{Fore.RESET}{Back.RESET}")
 else:
     print(f"{Fore.LIGHTRED_EX}{Back.LIGHTBLACK_EX}Wrong, the correct answer was {r * f}")
     print(f"Your score in total is {score}")
-
+    print(f"{Fore.RESET}{Back.RESET}")
 o = random.randint(0,5)
 p = random.randint(0,10)
 print(f"{Fore.RESET}{Back.RESET}What is {p} - {o}")
@@ -41,11 +43,13 @@ i = int(input("Answer Here: "))
 if (i) == (p) - (o):
     score += 1
     print(f"{Fore.BLUE}{Back.LIGHTGREEN_EX}Correct, your score is {score}!")
+    print(f"{Fore.RESET}{Back.RESET}")
 else:
     print(f"{Fore.LIGHTRED_EX}{Back.LIGHTBLACK_EX}Wrong.")
+    print(f"{Fore.RESET}{Back.RESET}")
 n = random.randint(1,10)
 m = random.randint(1,10)
-print(f"{Fore.RESET}{Back.RESET}What is {n} / {m}?")
+print(f"{Fore.RESET}{Back.RESET}What is {n} * {m}?")
 b = input("Answer Here: ")
 if (b) == (n) / (m):
     print(f"{Fore.BLUE}{Back.GREEN}Correct!")
