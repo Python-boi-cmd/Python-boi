2d8422ce25c99647816f4195ba3a7b2c1be9c865
python-boi! <cmd350.19@gmail.com>
2024-10-12 11:10:17 -0500

    Update Project15_app.py
    Math Streamlit

--- Project15_app.py
+++ Project15_app.py
@@ -3,17 +3,16 @@ import streamlit as st
 
 my_list = ["Addition", "Subtraction", "Multiplication", "Division"]
 
-
+st.set_page_config(page_title="Math Calculator", page_icon=":small_airplane:", layout="wide")
 item = st.selectbox("Select your Math Operation", my_list)
 
 x = st.number_input("Enter your number here: ")
 y = st.number_input("Enter your second number here: ")
-sum_result = x + y
-difference_result = x / y
-product_result = x * y
-division_result = x / y
-if division_result != 0:
-    st.write("Undefined number, cannot divide by zero")
-else:
-    st.write(f"The answer of {x} / {y} is {x / y}")
-
+if my_list == 0:
+    st.write(f"The sum of {x} + {y} is {x + y}.")
+elif my_list == 1:
+    st.write(f"The difference of {x} - {y} is {x - y}.")
+elif my_list == 2:
+    st.write(f"The product of {x} * {y} is {x * y}.")
+elif my_list == 3:
+    st.write(f"The quotient of {x} / {y} is {x / y}")
